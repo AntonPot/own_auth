613f112aa6d3ad918cb44b7a3f8a57d470904373
antonpot <markoapotocnik@hotmail.com>
2015-07-21 13:39:05 +0200

    Sessions#create finished, user can now log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,21 @@ class SessionsController < ApplicationController
   end
   def create
     email = params[:session][:email]
+    password = params[:session][:password]
     @user = User.find_by(email: email)
 
+    # authenticate returns user object if password mathces and false otherwise
+    if @user && @user.authenticate(password)
+      session[:user_id] = @user.id
+      redirect_to '/'
+    else
+      redirect_to '/login'
+    end
+
     p "$"*99
-    p @user
+    p session
     p "$"*99
+
   end
+
 end
