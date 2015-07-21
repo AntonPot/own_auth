212ba0d6f2a65bcbe650e1cef56776b8c9e87393
antonpot <markoapotocnik@hotmail.com>
2015-07-21 16:58:31 +0200

    Minor fix to validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   has_many :opinions
   has_many :surveys, through: :opinions
 
-  validates :email, confirmation: true
+  validates :email, :password, confirmation: true
   validates :first_name, :last_name, :username, :email, :email_confirmation, presence: true
   validates :username, uniqueness: true
 
