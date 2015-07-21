5771958b655da5d16e42795c78323ea4d8f3d925
antonpot <markoapotocnik@hotmail.com>
2015-07-21 17:32:50 +0200

    Email format validation added

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ActiveRecord::Base
   has_many :opinions
   has_many :surveys, through: :opinions
 
-  validates :email, :password, confirmation: true
+  validates :email, confirmation: true # doesn't need to vaildate password confirmation, it must be added to #has_secure_password
   validates :first_name, :last_name, :username, :email, :email_confirmation, presence: true
   validates :username, :email, uniqueness: true
 
-
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "must be an email address" }
 
 end
