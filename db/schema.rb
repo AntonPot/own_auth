4c8ce9789d2358361d965e563da601c852ebf609
antonpot <markoapotocnik@hotmail.com>
2015-07-22 22:12:01 +0200

    Username is take out from the application alltogther

--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,6 @@ ActiveRecord::Schema.define(version: 20150719075413) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.string   "username"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
