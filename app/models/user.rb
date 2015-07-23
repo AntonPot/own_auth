a6c329b2b34e0b4e0ccb3e9841bd894626e28516
antonpot <markoapotocnik@hotmail.com>
2015-07-23 10:27:32 +0200

    Username removed from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ActiveRecord::Base
   has_many :surveys, through: :opinions
 
   validates :email, confirmation: true # doesn't need to vaildate password confirmation, it must be added to #has_secure_password
-  validates :first_name, :last_name, :username, :email, :email_confirmation, presence: true
-  validates :username, :email, uniqueness: true
+  validates :first_name, :last_name, :email, :email_confirmation, presence: true
+  validates :email, uniqueness: true
 
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "must be an email address" }
 
