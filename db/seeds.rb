cec80f91c1eb3acfb383ba260280b2627308cbd9
antonpot <markoapotocnik@hotmail.com>
2015-07-23 10:17:18 +0200

    Username column removed from master as well

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,13 +13,11 @@ num_of_users.times do
   first_name = Faker::Name.first_name
   last_name  = Faker::Name.last_name
   email      = Faker::Internet.free_email("#{first_name}.#{last_name}")
-  username   = "#{first_name[0..3]}#{last_name[0..3]}#{(rand(99).to_s)}"
   password   = ['12345','54321'].sample
   user = User.new(
     first_name: first_name,
     last_name: last_name,
     email: email,
-    username: username
   )
   user.email_confirmation = email
   user.password = password
